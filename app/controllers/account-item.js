4098f52e45043bf9e962ff15b8fede98360321bc
Alexandre Michetti Manduca <a.michetti@gmail.com>
2014-05-14 14:46:45 -0300

    remove balance cache time

--- app/controllers/account-item.js
+++ app/controllers/account-item.js
@@ -1,15 +1,9 @@
 export default Ember.ObjectController.extend({
-  balanceUpdatedAt: null,
-
-  isBalanceCacheExpired: function() {
-    return account.get('balanceUpdatedAt') && (account.get('balanceUpdatedAt') - new Date()) > 5000;
-  },
-
   actions: {
     getBalance: function() {
       var account = this.get('model');
 
-      if (this.isBalanceCacheExpired()) {
+      if (account.get('balance')) {
         account.get('balance').reload();
 
       } else {
