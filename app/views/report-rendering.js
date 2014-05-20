bb6ae5ccaaaecfa253826cd94f3261eeb97c8738
Alexandre Michetti Manduca <a.michetti@gmail.com>
2014-05-19 21:59:01 -0300

    implement a different layout on report rendering

--- app/views/report-rendering.js
+++ app/views/report-rendering.js
@@ -1,4 +1,30 @@
 export default Ember.View.extend({
   tagName: 'div',
-  classNames: 'report-rendering'
+  classNames: 'report-rendering',
+
+  click: function(e) {
+    e.preventDefault();
+
+    // get clicked element
+    var $el = $(e.target);
+
+    // find category element
+    var $category = $el;
+    if (!$el.hasClass("report-rendering-category")) {
+      $category = $el.parents(".report-rendering-category").first();
+    }
+
+    // toggle icon
+    var $icon = $category.find(".glyphicon");
+    if ($icon.hasClass("glyphicon-collapse-up")) {
+      $icon.removeClass("glyphicon-collapse-up").addClass("glyphicon-collapse-down");
+    } else {
+      $icon.removeClass("glyphicon-collapse-down").addClass("glyphicon-collapse-up");
+    }
+
+    // toggle accounts table visibility
+    var $accounts = $category.find("> .accounts");
+    $accounts.slideToggle('fast');
+  }
+
 });
