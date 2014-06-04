a7ea595fd0fb95483aecb0d80fab6c811b5a7f57
Alexandre Michetti Manduca <a.michetti@gmail.com>
2014-06-04 14:38:16 -0300

    fix account lines index normal balance for balance formatting

--- app/controllers/account/lines/line.js
+++ app/controllers/account/lines/line.js
@@ -1,2 +1,4 @@
 export default Ember.ObjectController.extend({
+  needs: ['account/lines/index'],
+  normalBalance: Ember.computed.alias('controllers.account/lines/index.account.normalBalance')
 });
